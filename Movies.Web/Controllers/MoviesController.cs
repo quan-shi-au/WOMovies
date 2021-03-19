f31d0e9d5c001dd2ef202978ffb8e9752b183b61
Sam Quan <sam.quan@wontok.com>
2021-03-19 21:38:26 +1100

    add json-ld.net

--- Movies.Web/Controllers/MoviesController.cs
+++ Movies.Web/Controllers/MoviesController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JsonLD.Core;
+using Microsoft.AspNetCore.Mvc;
 using Movies.Web.Infrastructure;
 using Movies.Web.Models;
 using Movies.Web.Models.Movies;
 using Movies.Web.Services.Interfaces;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
 
 namespace Movies.Web.Controllers
@@ -18,6 +21,14 @@ namespace Movies.Web.Controllers
 
         public IActionResult Index()
         {
+
+            var json = "{'@context':{'test':'http://www.test.com/'},'test:hello':'world'}";
+            var document = JObject.Parse(json);
+            var expanded = JsonLdProcessor.Expand(document);
+
+            ViewBag.LdJsonObject = expanded;
+
+
             return View(new SearchResponse());
         }
 
@@ -29,7 +40,14 @@ namespace Movies.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(string title)
         {
+
+
             var searchResponse = await _movieApiService.SearchByTitle(title);
+
+            var json = JsonConvert.SerializeObject(searchResponse.Search);
+            var document = JObject.Parse(json);
+            var expanded = JsonLdProcessor.Expand(document);
+
 
             return View("Index", searchResponse);
         }
