a8f61bcee86746411990c411dcae95b331d0947c
Sam Quan <sam.quan@wontok.com>
2021-03-19 22:23:55 +1100

    test jsonld

--- Movies.Web/Controllers/MoviesController.cs
+++ Movies.Web/Controllers/MoviesController.cs
@@ -6,6 +6,7 @@ using Movies.Web.Models.Movies;
 using Movies.Web.Services.Interfaces;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Movies.Web.Controllers
@@ -21,51 +22,6 @@ namespace Movies.Web.Controllers
 
         public IActionResult Index()
         {
-
-            var _docJson = @"
-{
-    '@id': 'http://example.org/ld-experts',
-    'http://schema.org/name': 'LD Experts',
-    'http://schema.org/member': [{
-        '@type': 'http://schema.org/Person',
-        'http://schema.org/name': 'Manu Sporny',
-        'http://schema.org/url': {'@id': 'http://manu.sporny.org/'},
-        'http://schema.org/image': {'@id': 'http://manu.sporny.org/images/manu.png'}
-    },{
-        '@type': 'http://schema.org/Person',
-        'http://schema.org/name': 'Manu Sam',
-        'http://schema.org/url': {'@id': 'http://hi.sporny.org/'},
-        'http://schema.org/image': {'@id': 'http://xx.sporny.org/images/manu.png'}
-    }]
-}";
-
-            var _contextJson = @"
-{
-    'name': 'http://schema.org/name',
-    'member': 'http://schema.org/member',
-    'homepage': {'@id': 'http://schema.org/url', '@type': '@id'},
-    'image': {'@id': 'http://schema.org/image', '@type': '@id'},
-    'Person': 'http://schema.org/Person',
-    '@vocab': 'http://example.org/',
-    '@base': 'http://example.org/'
-}
-";
-
-            var doc = JObject.Parse(_docJson);
-            var context = JObject.Parse(_contextJson);
-            var opts = new JsonLdOptions();
-            var compacted = JsonLdProcessor.Compact(doc, context, opts);
-
-            ViewBag.LdJsonObject = compacted;
-
-
-            //var json = "{'@context':{'test':'http://www.test.com/'},'test:hello':'world'}";
-            //var document = JObject.Parse(json);
-            //var expanded = JsonLdProcessor.Expand(document);
-
-            //ViewBag.LdJsonObject = expanded;
-
-
             return View(new SearchResponse());
         }
 
@@ -77,16 +33,62 @@ namespace Movies.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(string title)
         {
-
-
             var searchResponse = await _movieApiService.SearchByTitle(title);
 
-            var json = JsonConvert.SerializeObject(searchResponse.Search);
-            var document = JObject.Parse(json);
-            var expanded = JsonLdProcessor.Expand(document);
-
+            SetJObject(searchResponse.Search);
 
             return View("Index", searchResponse);
+        }
+
+        private void SetJObject(List<MovieResponse> movies)
+        {
+
+            var movieList = @"
+{
+    '@id': 'http://example.org/movies',
+    'http://schema.org/name': 'Movies',
+    'http://schema.org/items': [{
+        '@type': 'http://schema.org/Movie',
+        'http://schema.org/name': 'Manu Sporny',
+        'http://schema.org/year': {'@id': 'http://manu.sporny.org/'},
+        'http://schema.org/image': {'@id': 'http://manu.sporny.org/images/manu.png'}
+    },{
+        '@type': 'http://schema.org/Movie',
+        'http://schema.org/name': 'Manu Sam',
+        'http://schema.org/year': {'@id': 'http://hi.sporny.org/'},
+        'http://schema.org/image': {'@id': 'http://xx.sporny.org/images/manu.png'}
+    }";
+
+            foreach (var movie in movies)
+            {
+                movieList += @"
+,{
+        '@type': 'http://schema.org/Movie',
+        'http://schema.org/name': '" + movie.Title.Replace("'", "") + @"',
+        'http://schema.org/year': {'@id': '" + movie.Year + @"'},
+        'http://schema.org/image': {'@id': '" + movie.Poster + @"'}
+    }
+";
+            }
+
+movieList += "]}";
+
+            var _contextJson = @"
+{
+    'name': 'http://schema.org/name',
+    'member': 'http://schema.org/items',
+    'homepage': {'@id': 'http://schema.org/year', '@type': '@id'},
+    'image': {'@id': 'http://schema.org/image', '@type': '@id'},
+    'Movie': 'http://schema.org/Movie'
+}
+";
+
+            var doc = JObject.Parse(movieList);
+            var context = JObject.Parse(_contextJson);
+            var opts = new JsonLdOptions();
+            var compacted = JsonLdProcessor.Compact(doc, context, opts);
+
+            ViewBag.LdJsonObject = compacted;
         }
 
     }
