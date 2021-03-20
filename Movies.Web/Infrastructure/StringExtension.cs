6e63239f31b6e7406a570209944f426b328e0856
Sam Quan <sam.quan@wontok.com>
2021-03-20 11:42:15 +1100

    jsonld for detail

---
+++ Movies.Web/Infrastructure/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Web.Infrastructure
+{
+    public static class StringExtension
+    {
+        public static string EscapeString(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            return input.Replace("'", "\"");
+        }
+    }
+}
--- Movies.Web/Services/JsonLdService.cs
+++ Movies.Web/Services/JsonLdService.cs
@@ -1,4 +1,5 @@
 ﻿using JsonLD.Core;
+using Movies.Web.Infrastructure;
 using Movies.Web.Models;
 using Movies.Web.Services.Interfaces;
 using Newtonsoft.Json.Linq;
@@ -32,7 +33,7 @@ namespace Movies.Web.Services
                 movieList += @"
                 {
                     '@type': 'http://schema.org/Movie',
-                    'http://schema.org/name': '" + movie.Title.Replace("'", "") + @"',
+                    'http://schema.org/name': '" + movie.Title.EscapeString() + @"',
                     'http://schema.org/year': {'@id': '" + movie.Year + @"'},
                     'http://schema.org/image': {'@id': '" + movie.Poster + @"'}
                 }
@@ -63,17 +64,59 @@ namespace Movies.Web.Services
         {
             var _docJson = @"
 {
-    '@id': 'id-1234',
-    'http://schema.org/title': 'test',
-        '@type': 'http://schema.org/Movie',
-'http://schema.org/language': 'English'
+    '@id': '" + movie.imdbID + @"',
+        '@type': 'Movie Detail',
+    'http://schema.org/Title': '" + movie.Title.EscapeString() + @"',
+    'http://schema.org/Year': '" + movie.Year.EscapeString() + @"',
+    'http://schema.org/Rated': '" + movie.Rated.EscapeString() + @"',
+    'http://schema.org/Released': '" + movie.Released.EscapeString() + @"',
+    'http://schema.org/Runtime': '" + movie.Runtime.EscapeString() + @"',
+    'http://schema.org/Genre': '" + movie.Genre.EscapeString() + @"',
+    'http://schema.org/Director': '" + movie.Director.EscapeString() + @"',
+    'http://schema.org/Writer': '" + movie.Writer.EscapeString() + @"',
+    'http://schema.org/Actors': '" + movie.Actors.EscapeString() + @"',
+    'http://schema.org/Plot': '" + movie.Plot.EscapeString() + @"',
+    'http://schema.org/Language': '" + movie.Language.EscapeString() + @"',
+    'http://schema.org/Country': '" + movie.Country.EscapeString() + @"',
+    'http://schema.org/Awards': '" + movie.Awards.EscapeString() + @"',
+    'http://schema.org/Poster': '" + movie.Poster.EscapeString() + @"',
+    'http://schema.org/Metascore': '" + movie.Metascore.EscapeString() + @"',
+    'http://schema.org/imdbRating': '" + movie.imdbRating.EscapeString() + @"',
+    'http://schema.org/imdbVotes': '" + movie.imdbVotes.EscapeString() + @"',
+    'http://schema.org/imdbID': '" + movie.imdbID.EscapeString() + @"',
+    'http://schema.org/DVD': '" + movie.DVD.EscapeString() + @"',
+    'http://schema.org/BoxOffice': '" + movie.BoxOffice.EscapeString() + @"',
+    'http://schema.org/Production': '" + movie.Production.EscapeString() + @"',
+    'http://schema.org/Website': '" + movie.Website.EscapeString() + @"',
+    'http://schema.org/Response': '" + movie.Response.EscapeString() + @"'
 }
 ";
 
             var _contextJson = @"
 {
-    'title': 'http://schema.org/title',
-    'language': 'http://schema.org/language',
+    'Title': 'http://schema.org/Title',
+    'Year': 'http://schema.org/Year',
+    'Rated': 'http://schema.org/Rated',
+    'Released': 'http://schema.org/Released',
+    'Runtime': 'http://schema.org/Runtime',
+    'Genre': 'http://schema.org/Genre',
+    'Director': 'http://schema.org/Director',
+    'Writer': 'http://schema.org/Writer',
+    'Actors': 'http://schema.org/Actors',
+    'Plot': 'http://schema.org/Plot',
+    'Language': 'http://schema.org/Language',
+    'Country': 'http://schema.org/Country',
+    'Awards': 'http://schema.org/Awards',
+    'Poster': 'http://schema.org/Poster',
+    'Metascore': 'http://schema.org/Metascore',
+    'imdbRating': 'http://schema.org/imdbRating',
+    'imdbVotes': 'http://schema.org/imdbVotes',
+    'imdbID': 'http://schema.org/imdbID',
+    'DVD': 'http://schema.org/DVD',
+    'BoxOffice': 'http://schema.org/BoxOffice',
+    'Production': 'http://schema.org/Production',
+    'Website': 'http://schema.org/Website',
+    'Response': 'http://schema.org/Response'
 }";
 
             var doc = JObject.Parse(_docJson);
